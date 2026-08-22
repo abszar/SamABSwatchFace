6e299170d61bc27a9da714d86e18771cce1e2ec9
Abdelali <abdelali@dar-elweb.com>
2026-08-22 18:11:22 +0200

    feat: scaffold WFF v2 watch face project

---
+++ watchface/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.android.application") version "8.5.2"
+}
+
+android {
+    namespace = "com.abszar.samabwatchface"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.abszar.samabwatchface"
+        minSdk = 33
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
