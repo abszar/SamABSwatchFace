a62fb9a2cc670a8f6f64a5783881497a10e3a099
Abdelali <abdelali@dar-elweb.com>
2026-08-22 19:57:28 +0200

    feat: seconds counter blue and closer to minutes

--- watchface/build.gradle.kts
+++ watchface/build.gradle.kts
@@ -9,7 +9,7 @@ android {
         applicationId = "com.abszar.samabwatchface"
         minSdk = 33
         targetSdk = 34
-        versionCode = 9
+        versionCode = 10
         versionName = "1.6"
     }
 }
