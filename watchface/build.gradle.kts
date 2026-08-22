386a50eb6bb96120e7b4777e7e1fd44030b2ded2
Abdelali <abdelali@dar-elweb.com>
2026-08-22 19:29:49 +0200

    feat: raise date line and enlarge clock to 106px

--- watchface/build.gradle.kts
+++ watchface/build.gradle.kts
@@ -9,7 +9,7 @@ android {
         applicationId = "com.abszar.samabwatchface"
         minSdk = 33
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
 }
