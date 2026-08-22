9973cc16513d7cac9c825a0ff3acfa1b6fe2ebb1
Abdelali <abdelali@dar-elweb.com>
2026-08-22 21:05:59 +0200

    feat: replace sleep icon with transparent moon artwork

--- watchface/build.gradle.kts
+++ watchface/build.gradle.kts
@@ -9,7 +9,7 @@ android {
         applicationId = "com.abszar.samabwatchface"
         minSdk = 33
         targetSdk = 34
-        versionCode = 25
+        versionCode = 26
         versionName = "1.6"
     }
 }
